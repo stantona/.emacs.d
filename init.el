d0fc3c846c9077ff3e45a37a56117d9ef947c00a
Adam Stanton <adamstanton1979@gmail.com>
2015-03-14 18:25:03 -0700

    Changes to init.el
    - Add yaml-mode
    - Change font to Monospace

--- init.el
+++ init.el
@@ -28,7 +28,8 @@
    move-text
    ample-theme
    web-mode
-   sublime-themes))
+   sublime-themes
+   yaml-mode))
 
 ;; Initialize ido mode
 (require 'init-ido)
@@ -73,15 +74,15 @@
 
 ;; Set the font
 (add-to-list 'default-frame-alist
-             '(font . "Terminus-12"))
+             '(font . "Monospace-10"))
 
-(set-default-font "Terminus-11")
+(set-default-font "Monospace-10")
 
 ;; Turn off tab indentation and use 2 spaces for width
 (setq-default indent-tabs-mode nil)
 (setq tab-width 2)
 
-(load-theme 'fogus t)
+(load-theme 'base16-railscasts t)
 
 ;; Show the file name and path in the frame's title
 (setq frame-title-format
@@ -91,3 +92,15 @@
 
 ;; Use ibuffer
 (defalias 'list-buffers 'ibuffer)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("c537bf460334a1eca099e05a662699415f3971b438972bed499c5efeb821086b" "e3c90203acbde2cf8016c6ba3f9c5300c97ddc63fcb78d84ca0a144d402eedc6" "7b7ef508f9241c01edaaff3e0d6f03588a6f4fddb1407a995a7a333b29e327b5" "a0fdc9976885513b03b000b57ddde04621d94c3a08f3042d1f6e2dbc336d25c7" "8f1cedf54f137f71382e3367e1843d10e173add99abe3a5f7d3285f5cc18f1a9" "2a86b339554590eb681ecf866b64ce4814d58e6d093966b1bf5a184acf78874d" "8022cea21aa4daca569aee5c1b875fbb3f3248a5debc6fc8cf5833f2936fbb22" "0ed983facae99849805b2f7be926561cb58474eb18e5296d9bb3ad7f9b088a5b" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
