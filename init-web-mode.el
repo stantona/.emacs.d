77f5d67a4dd181ba21b3df734ce6d1b343342565
Adam Stanton <adamstanton1979@gmail.com>
2015-03-26 17:38:41 -0700

    Update web-mode configuration. Use sane indentation.

--- init-web-mode.el
+++ init-web-mode.el
@@ -3,4 +3,12 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+(defun ajs-web-mode-hook ()
+  "Hooks for web mode"
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+)
+
+(add-hook 'web-mode-hook 'ajs-web-mode-hook)
+(add-hook 'web-mode-hook 'projectile-mode)
 (provide 'init-web-mode)
