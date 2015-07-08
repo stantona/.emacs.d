d2afa968954fe877bed1ad85dd0a61ae1dd98bc6
Adam Stanton <adamstanton1979@gmail.com>
2015-07-08 04:49:19 -0700

    Set ENABLE_PRY_RAILS variable in the init-ruby.el

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -1,5 +1,8 @@
 (chruby "2.1.4")
 
+;; Enable pry for rails
+(setenv "ENABLE_PRY_RAILS" "1")
+
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . ruby-mode))
